17093ac4039fa5825cf5d86f3a4912f3f0bad20c
ngactive99 <ngactive99@gmail.com>
2022-04-26 12:43:23 +0530

    get method implemented

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-const mysql = require('mysql');
-const express = require('express');
+const mysql = require("mysql");
+const express = require("express");
 var app = express();
-const bodyparser = require('body-parser');
-
+const bodyparser = require("body-parser");
 
 app.use(bodyparser.json());
 
 var mysqlConnection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'root',
-    database: 'EmployeeDB'
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "EmployeeDB",
 });
 
 mysqlConnection.connect((err) => {
-    if(!err) 
-    console.log('db connection success' );
-    else
-    console.log('db connection failed \n ' + JSON.stringify(err, undefined, 2));
-})
+  if (!err) console.log("db connection success");
+  else
+    console.log("db connection failed \n " + JSON.stringify(err, undefined, 2));
+});
+
+app.listen(3000, () => console.log("Express server is running at port 3000"));
+
+app.get("/employees", (res, req) => { // get req , in terms of crud the R that stands of read
+  mysqlConnection.query("SELECT * fROM Employee", (err, rows, feilds) => {
+    if (!err) console.log(rows);
+    else console.log(err);
+  });
+});
