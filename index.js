4596f425714a682161e0ab39de78fff0de4c1bcf
ngactive99 <ngactive99@gmail.com>
2022-04-27 11:48:35 +0530

    put and post operations

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var mysqlConnection = mysql.createConnection({
   user: "root",
   password: "root",
   database: "EmployeeDB",
+  multipleStatements: true,
 });
 
 mysqlConnection.connect((err) => {
@@ -21,27 +22,72 @@ mysqlConnection.connect((err) => {
 app.listen(3000, () => console.log("Express server is running at port 3000"));
 
 //retirved employees from the database
-app.get("/employees", (req, res ) => { // get req , in terms of crud the R that stands of read
-  mysqlConnection.query("SELECT * fROM Employee", (err, rows, feilds) => {
+app.get("/employees", (req, res) => {
+  // get req , in terms of crud the R that stands of read
+  mysqlConnection.query("SELECT * fROM Employee", (err, rows, fields) => {
     if (!err) res.send(rows);
     else console.log(err);
   });
 });
 
-// get sepcific employee using the id 
+// get sepcific employee using the id
 // localhost:3000/employee/2 => the details of empoloyee with the id 2
-app.get("/employee/:id", (req, res ) => { 
-  mysqlConnection.query("SELECT * fROM Employee WHERE EmpID =? ",[req.params.id], (err, rows, feilds) => {
-    if (!err) res.send(rows);
-    else console.log(err);
-  });
+app.get("/employee/:id", (req, res) => {
+  mysqlConnection.query(
+    "SELECT * fROM Employee WHERE EmpID =? ",
+    [req.params.id],
+    (err, rows, fields) => {
+      if (!err) res.send(rows);
+      else console.log(err);
+    }
+  );
 });
 
-// delete employee with regards to the id 
-// localhost:3000/deleteemployee/2 => this will delete the employee with the id of 2 
-app.delete("/employee/:id", (req, res ) => { 
-  mysqlConnection.query("DELETE fROM Employee WHERE EmpID =? ",[req.params.id], (err, rows, feilds) => {
-    if (!err) res.send("SUCESSFULLY DELETED EMPLOYEE!");
-    else console.log(err);
-  });
+// delete employee with regards to the id
+// localhost:3000/employee/2 => this will delete the employee with the id of 2
+app.delete("/employee/:id", (req, res) => {
+  mysqlConnection.query(
+    "DELETE FROM Employee WHERE EmpID =? ",
+    [req.params.id],
+    (err, rows, feilds) => {
+      if (!err) res.send("SUCESSFULLY DELETED EMPLOYEE! " + [req.params.id]);
+      else console.log(err);
+    }
+  );
+});
+
+// WE ARE GOING TO INSERT AN EMPLOYEE
+//POST OPERATION
+app.post("/employees", (req, res) => {
+  let emp = req.body;
+  var sql ="SET @EmpID = ?; SET @Name = ?; SET @EmpCode = ?; SET @Salary = ?; \
+   CALL EmployeeAddOrEdit(@EmpID,@Name,@EmpCode,@Salary)";
+  mysqlConnection.query(
+    sql,
+    [emp.EmpID, emp.Name, emp.EmpCode, emp.Salary],
+    (err, rows, fields) => {
+      if (!err)
+        rows.forEach(element => {
+          if (element.constructor == Array);
+          res.send("Inserted employee and the id = " + element[0]);
+        });
+      else 
+      console.log(err);
+    });
+});
+
+// here we are going to update the pre existing data
+app.put("/employees", (req, res) => {
+  let emp = req.body;
+  var sql ="SET @EmpID = ?; SET @Name = ?; SET @EmpCode = ?; SET @Salary = ?; \
+   CALL EmployeeAddOrEdit(@EmpID,@Name,@EmpCode,@Salary)";
+  mysqlConnection.query(
+    sql,
+    [emp.EmpID, emp.Name, emp.EmpCode, emp.Salary],
+    (err, rows, fields) => {
+      if (!err)
+       res.send("updated sucessfully");
+      else 
+      console.log(err);
+    });
 });
